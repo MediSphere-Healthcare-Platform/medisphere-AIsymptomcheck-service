98095d64feb893eb9889846ab68a88f6d8421ba6
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 16:17:49 +0530

    initial commit

---
+++ src/main/java/com/medisphere/AIsymptomcheck/MedisphereAIsymptomcheckServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.AIsymptomcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisphereAIsymptomcheckServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisphereAIsymptomcheckServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medisphere/AIsymptomcheck/MedisphereAIsymptomcheckServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisphere.AIsymptomcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisphereAIsymptomcheckServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
