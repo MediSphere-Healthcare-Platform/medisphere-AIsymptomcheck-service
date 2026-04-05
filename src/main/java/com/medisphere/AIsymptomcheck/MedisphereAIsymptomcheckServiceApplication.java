4087c7759e743724df5f9bc249a4cc2287c306fe
AroshXR <aroshanasandeep454@gmail.com>
2026-04-05 22:02:40 +0530

    application.properties

--- src/main/java/com/medisphere/AIsymptomcheck/MedisphereAIsymptomcheckServiceApplication.java
+++ src/main/java/com/medisphere/AIsymptomcheck/MedisphereAIsymptomcheckServiceApplication.java
@@ -1,9 +1,10 @@
 package com.medisphere.AIsymptomcheck;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedisphereAIsymptomcheckServiceApplication {
 
 	public static void main(String[] args) {
